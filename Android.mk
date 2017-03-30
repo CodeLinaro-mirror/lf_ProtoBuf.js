af888c233b36e86ed78d816712249b5cd38fd966
Yogendra Charya Tangalapally <yogendra@codeaurora.org>
2017-03-30 23:12:47 +0530

    Revert "Commented make file for Kai2.0 binaries compilation"
    This reverts commit fdea5194197e2f325a451a02d564337b11fef998.

--- Android.mk
+++ Android.mk
@@ -1,13 +1,13 @@
 # Only export in debug builds
-#ifeq ($(filter user, $(TARGET_BUILD_VARIANT)),)
+ifeq ($(filter user, $(TARGET_BUILD_VARIANT)),)
 
-#LOCAL_PATH:= $(call my-dir)
-#include $(CLEAR_XPCOM_VARS)
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_XPCOM_VARS)
 
-#LOCAL_MODULE := Protobuf.js
-#LOCAL_MODULE_TAGS := optional
-#LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-#LOCAL_SRC_FILES := dist/ProtoBuf.js
+LOCAL_MODULE := Protobuf.js
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_SRC_FILES := dist/ProtoBuf.js
 
-#include $(BUILD_XPCOM)
-#endif
+include $(BUILD_XPCOM)
+endif
